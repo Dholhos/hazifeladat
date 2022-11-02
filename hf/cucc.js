fee48bdb70c8cdab956b5749423aed87fdbe1455
Dholhos <113104720+Dholhos@users.noreply.github.com>
2022-11-02 09:42:20 +0100

    Add files via upload

---
+++ hf/cucc.js
@@ -0,0 +1,11 @@
+function cucc(){
+
+let elso = document.getElementById("Mnev").value;
+document.getElementById("vnev").innerHTML ="Név:" + elso;
+
+let masodik = document.getElementById("Mcim").value;
+document.getElementById("vcim").innerHTML ="Cím:" + masodik;
+
+let harmadik = document.getElementById("Mtele").value;
+document.getElementById("vtele").innerHTML ="Telefon:" + harmadik;
+}
